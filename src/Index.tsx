90384b09f57c3c1f33bc312d2b9930e3351f9497
Patrick Siegfried <siegfried.patrick@gmail.com>
2016-11-11 16:35:14 +0100

    - so far without dagre layout...

--- src/Index.tsx
+++ src/Index.tsx
@@ -6,21 +6,75 @@ import * as injectTapEventPlugin from "react-tap-event-plugin";
 import {MuiThemeProvider} from "material-ui/styles";
 import getMuiTheme = __MaterialUI.Styles.getMuiTheme;
 import {Paper} from "material-ui";
+let Cytoscape = require('cytoscape');
+let dagre = require('dagre');
+import {View} from "ikc-visual-interfaces/src/common/interfaces/View";
 
 init();
 
 function init() {
-    /** Init need tap eventplug of material-ui */
-    injectTapEventPlugin();
 
-    ReactDOM.render((
-        <div id="wrapper">
-            <MuiThemeProvider>
-                <Paper zDepth={1}>
-                    <h1>Empty Template</h1>
-                </Paper>
-            </MuiThemeProvider>
-        </div>
-    ), document.getElementById("content"))
+    let cy = (window as any).cy = Cytoscape({
+        container: document.getElementById('ikc-visual'),
+
+        boxSelectionEnabled: false,
+        autounselectify: true,
+
+        /*layout: {
+            name: 'dagre'
+        },*/
+
+        style: [
+            {
+                selector: 'node',
+                style: {
+                    'content': 'data(id)',
+                    'text-opacity': 0.5,
+                    'text-valign': 'center',
+                    'text-halign': 'right',
+                    'background-color': '#11479e'
+                }
+            },
+
+            {
+                selector: 'edge',
+                style: {
+                    'width': 4,
+                    //'label': 'data(label)',
+                    'target-arrow-shape': 'triangle',
+                    'line-color': '#9dbaea',
+                    'target-arrow-color': '#9dbaea',
+                    'curve-style': 'bezier'
+                }
+            }
+        ],
+
+        elements: {
+            nodes: [
+                {data: {id: 'home'}},
+                {data: {id: 'ikc'}},
+                {data: {id: 'pawi'}},
+                {data: {id: 'bda'}},
+            ],
+            edges: [
+                {data: {source: 'home', target: 'ikc', label: 'project'}},
+                {data: {source: 'ikc', target: 'home', label: 'backlink'}},
+                {data: {source: 'home', target: 'pawi', label: 'project'}},
+            ]
+        },
+
+    });
 
 }
+/** Init need tap eventplug of material-ui */
+injectTapEventPlugin();
+
+ReactDOM.render((
+    <div id="wrapper">
+        <MuiThemeProvider>
+            <Paper zDepth={1}>
+                <h1>Empty Template</h1>
+            </Paper>
+        </MuiThemeProvider>
+    </div>
+), document.getElementById("content"));
