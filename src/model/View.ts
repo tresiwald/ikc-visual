0646fdc4223b34e6d13d145910c7dcc951b36afb
Andreas Waldis <andreas.waldis@stud.hslu.ch>
2017-01-16 09:43:14 +0100

    Update required packages

--- src/model/View.ts
+++ src/model/View.ts
@@ -5,11 +5,15 @@ import {TimeService} from "../common/TimeService";
 export class View{
     id:string
     title:string
+    changedAt:string
+    createdAt:string
     nodes: GraphNodeElement[]
     links: GraphLinkElement[]
 
     constructor(title:string, nodes: GraphNodeElement[], links: GraphLinkElement[]){
         this.id = TimeService.getTimestamp()
+        this.changedAt = TimeService.getTimestamp()
+        this.createdAt = TimeService.getTimestamp()
         this.nodes = nodes
         this.links = links
     }
