941d579ddf0e3d95c6015097b6a517c1a61b0996
Andreas Waldis <andreas.waldis@stud.hslu.ch>
2017-01-16 09:18:41 +0100

    Update required packages

--- src/model/ViewFactory.ts
+++ src/model/ViewFactory.ts
@@ -1,7 +1,12 @@
 import {View} from "./View";
+import {GraphNodeElement} from "./GraphNodeData";
+import {GraphLinkElement} from "./GraphLinkData";
 
 export module ViewFactory {
     export function initViewFromJson(json: string):View {
         return (JSON.parse(json) as View)
+    }
+    export function viewFromNodesAndLinks(nodes: GraphNodeElement[], links: GraphLinkElement[]): View{
+        return new View(nodes, links)
     }
 }
