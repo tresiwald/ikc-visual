0446c62e6b359ef4f865d3fe38ef8f2c6ca5703f
Andreas Waldis <andreas.waldis@stud.hslu.ch>
2017-01-16 18:09:50 +0100

    Update required packages

--- src/interfaces/OperationService.ts
+++ src/interfaces/OperationService.ts
@@ -4,9 +4,9 @@ import {View} from "../model/View";
  */
 export interface OperationService {
     createNode(id: string, label: string): void;
-    createLink(id: string, source: string, targets: string, label: string): void;
+    createLink(id: string, source: string, target: string, label: string): void;
     updateNode(id: string, label: string): void;
-    updateLink(id: string, source: string, targets: string, label: string): void;
+    updateLink(id: string, source: string, target: string, label: string): void;
     deleteNode(id: string): void;
     deleteLink(id: string): void;
     saveView(view:View):void;
